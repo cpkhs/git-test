cccda3457499f8854e3bc211d9c5df5f2d2a6b95
andrew <softsun5257@163.com>
2022-08-07 08:05:39 +0800

    second commit

--- src/main/java/com/softsun/git/GitTest.java
+++ src/main/java/com/softsun/git/GitTest.java
@@ -9,6 +9,8 @@ package com.softsun.git;
  **/
 public class GitTest {
     public static void main(String[] args) {
+
         System.out.println("hello world");
+        System.out.println("hello world2");
     }
 }
