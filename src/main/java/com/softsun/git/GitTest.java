092ffd2bffd55a307e977f53adc0bb6341dd1979
andrew <softsun5257@163.com>
2022-08-07 08:02:35 +0800

    commit

---
+++ src/main/java/com/softsun/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.softsun.git;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO softsun
+ * @Author Andrew
+ * @Date 2022/8/7  7:45
+ * @Version 1.0
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
